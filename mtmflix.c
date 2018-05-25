431f03f3b843acd05f33c37993fd12babdead58c
moira <mashashkapa@gmail.com>
2018-05-25 17:16:10 +0300

    Add files via upload

---
+++ mtmflix.c
@@ -0,0 +1,101 @@
+//Add ifndef?
+
+#include <stdlib.h>
+#include "libmtm.h"
+#include "libmtm_ex3.h"
+
+
+struct mtmFlix_t {
+	Name name;
+	Series series;
+	User user;
+};
+
+MtmFlix mtmFlixCreate();
+void mtmFlixDestroy(MtmFlix mtmflix);
+
+MtmFlixResult mtmFlixAddSeries(MtmFlix mtmflix, const char* name, int episodesNum, Genre genre, int* ages, int episodesDuration);
+MtmFlixResult mtmFlixRemoveSeries(MtmFlix mtmflix, const char* name);
+MtmFlixResult mtmFlixSeriesJoin(MtmFlix mtmflix, const char* username, const char* seriesName);
+MtmFlixResult mtmFlixSeriesLeave(MtmFlix mtmflix, const char* username, const char* seriesName);
+
+MtmFlixResult mtmFlixAddUser(MtmFlix mtmflix, const char* username, int age);
+MtmFlixResult mtmFlixRemoveUser(MtmFlix mtmflix, const char* username);
+MtmFlixResult mtmFlixAddFriend(MtmFlix mtmflix, const char* username1, const char* username2);
+MtmFlixResult mtmFlixRemoveFriend(MtmFlix mtmflix, const char* username1, const char* username2);
+MtmFlixResult mtmFlixGetRecommendations(MtmFlix mtmflix, const char* username, int count, FILE* outputStream);
+
+MtmFlixResult mtmFlixReportSeries(MtmFlix mtmflix, int seriesNum, FILE* outputStream);
+MtmFlixResult mtmFlixReportUsers(MtmFlix mtmflix, FILE* outputStream);
+
+
+MtmFlix mtmFlixCreate() {
+	//create a network
+	MtmFlix mtmflix = malloc(sizeof(MtmFlix));
+	mtmflix->user = mapCreate();
+	mtmflix->series = mapCreate();
+	//проверить, что они не возвращают ошибку
+	//возможно, сначала инициализировать их на NULL?
+	return mtmflix;
+	//errors
+}
+
+void mtmFlixDestroy(MtmFlix mtmflix) {
+	//DESTROY ALL FIELDS
+	mapDestroy(mtmflix->user);
+	mapDestroy(mtmflix->series);
+	free(mtmflix);
+	mtmflix = NULL;
+	//destroy a network
+}
+
+MtmFlixResult mtmFlixAddSeries(MtmFlix mtmflix, const char* name, int episodesNum, Genre genre, int* ages, int episodesDuration) {
+	Series series = SeriesCreate(episodesNum, genre, ages, episodesDuration);
+	if (mapPut(mtmflix->series, name, series) == //проверить статусы, поступить соответствующим образом
+}
+
+MtmFlixResult mtmFlixRemoveSeries(MtmFlix mtmflix, const char* name) {
+	mapRemove(mtmflix, name);
+}
+
+MtmFlixResult mtmFlixSeriesJoin(MtmFlix mtmflix, const char* username, const char* seriesName) {
+	UserAddFavSeries(username, seriesName);
+}
+
+MtmFlixResult mtmFlixSeriesLeave(MtmFlix mtmflix, const char* username, const char* seriesName) {
+	//delete from dictionary via user's function
+}
+
+MtmFlixResult mtmFlixAddUser(MtmFlix mtmflix, const char* username, int age) {
+	User user = UserCreate(age);
+	if (mapPut mtmflix->users(mtmflix, username, user) ==
+}
+
+MtmFlixResult mtmFlixRemoveUser(MtmFlix mtmflix, const char* username);
+
+MtmFlixResult mtmFlixAddFriend(MtmFlix mtmflix, const char* username1, const char* username2) {
+	UserAddFriend(mapGet(mtmflix->user, username1), username2);
+}
+
+MtmFlixResult mtmFlixRemoveFriend(MtmFlix mtmflix, const char* username1, const char* username2) {
+	UserRemoveFriend(mapGet(mtmflix->user, username1), username2);
+}
+
+MtmFlixResult mtmFlixGetRecommendations(MtmFlix mtmflix, const char* username, int count, FILE* outputStream);
+
+MtmFlixResult mtmFlixReportSeries(MtmFlix mtmflix, int seriesNum, FILE* outputStream);
+
+MtmFlixResult mtmFlixReportUsers(MtmFlix mtmflix, FILE* outputStream);
+
+
+static SeriesMapToList (Map map, Type type) {
+	List list = listCreate(copy, destroy);
+	mapGetFirst(Map map);
+	MAP_FOREACH(type,iterator,map) {
+
+	}
+	//empty list
+	//for series in series
+	//add series contents to list
+}
+
---
+++ series.c
@@ -0,0 +1,19 @@
+//ifndef include
+
+struct mtmFlix_t {
+	//Name name;
+	int episodesNum;
+	Genre genre;
+	int* ages;
+	int episodesDuration;
+};
+
+Series SeriesCreate (const char* name, int episodesNum, Genre genre, int* ages, int episodesDuration) {
+	Series series = malloc(sizeof(Series));
+	//series-> name = name;
+	series->episodesNum = episodesNum;
+	series->genre = genre;
+	series ages = ages;
+	series->episodesDuration = episodesDuration;
+	return series;
+}

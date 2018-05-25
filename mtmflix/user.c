49a96a30133061795233effb7f0faa6254fe1711
PURSH98 <gufm.se@gmail.com>
2018-05-25 20:07:39 +0300

    adding folder which contains only 'c' files

---
+++ mtmflix/user.c
@@ -0,0 +1,19 @@
+//ifndef include
+
+struct user_t {
+	Age age;
+	Series fav_series;
+	Friends friends;
+};
+
+MtmFlixResult UserAddFavSeries (User user, Series series) {
+	//add to dict
+}
+
+MtmFlixResult UserAddFriend(User user1, User user2) {
+
+}
+
+MtmFlixResult UserRemoveFriend(User user1, User user2) {
+
+}

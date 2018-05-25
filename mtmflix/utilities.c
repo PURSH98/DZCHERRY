29199ae997aa9be172400736f55f00e7ba5cd0a1
moira <mashashkapa@gmail.com>
2018-05-25 20:32:56 +0300

    Update utilities.c

--- mtmflix/utilities.c
+++ mtmflix/utilities.c
@@ -1,1 +1,11 @@
 //see header for explanation
+static ??? SeriesMapToList (Map map, Type type) { //what does it return? we have no list types
+	List list = listCreate(copy, destroy);
+	mapGetFirst(Map map);
+	MAP_FOREACH(type,iterator,map) { //FOREACH example https://moodle.technion.ac.il/mod/hsuforum/discuss.php?d=397
+	
+	}
+	//empty list
+	//for series in series
+	//add series contents to list
+}

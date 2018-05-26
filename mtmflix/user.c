9212c7a6c44a263cc566569b9fd9d68ad9d0323d
Moira <moira@yandex.ru>
2018-05-26 14:06:36 +0300

    more stuff

--- mtmflix/user.c
+++ mtmflix/user.c
@@ -12,8 +12,8 @@ User userCreate(int age, Series fav_series, Friends friends) {
 		return NULL;
 	}
 	user->age = age;
-	user->fav_series = fav_series;
-	user->friends = friends;
+	user->fav_series = setCreate(copySeriesElement, freeSeriesElement, compareSeriesElements);
+	user->friends = setCreate(copyFriendElement, freeFriendElement, compareFriendElements);
 	return user;
 }
 
@@ -21,7 +21,9 @@ User userCopy (User user) {
 	if (user == NULL) {
 		return NULL;
 	}
-	User new_user = userCreate(user->age, user->fav_series, user->friends);
+	User new_user = malloc(User);
+	new_user->age = user->age;
+	//copy series or just pointer? the same about friends
 	if (new_user == NULL) {
 		return NULL;
 	}
@@ -29,17 +31,43 @@ User userCopy (User user) {
 }
 
 void userFree(User user) {
-	user->fav_series = NULL; //we don't need to delete other users or series
-	user->friends = NULL;
+	user->fav_series = NULL;
 	free(user);
 	user = NULL;
 }
 
-MtmFlixResult userAddFavSeries (User user, Series series) {
-
+MtmFlixResult userAddFavSeries (User user, SeriesElement series_element) {
+	//series must be a series element here
+	setAdd(user->fav_series, series_element);
+	//errors
 }
 
 MtmFlixResult userAddFriend(User user1, User user2) {
+
+}
+
+SeriesElement copySeriesElement {
+
+}
+
+void freeSeriesElement(SeriesElement series_element) {
+
+}
+
+int compareSeriesElements(SeriesElement series_element_a, SeriesElement series_element_b) {
+
+}
+
+
+SeriesElement copyFriendElement {
+
+}
+
+void freeFriendElement(SeriesElement series_element) {
+
+}
+
+int compareFriendElements(SeriesElement series_element_a, SeriesElement series_element_b) {
 
 }
 

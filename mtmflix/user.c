806217c5e7bce677ed53c35661c6d35cb8e42c89
Moira <moira@yandex.ru>
2018-05-28 00:07:08 +0300

    comments

--- mtmflix/user.c
+++ mtmflix/user.c
@@ -11,6 +11,7 @@ struct user_t {
 };
 
 //creates a user instance given user's age
+//it will be used as a value in a key-value pair in a map of users
 User userCreate(int age) {
 	User user = malloc(sizeof(User));
 	if (user == NULL) {
@@ -24,50 +25,64 @@ User userCreate(int age) {
 	return user;
 }
 
+//creater a copy of a user instance, returns a new user
 User userCopy (User user) {
 	if (user == NULL) {
 		return NULL;
 	}
 	User new_user = malloc(sizeof(User));
 	new_user->age = user->age;
-	//copy series or just pointer? the same about friends
 	if (new_user == NULL) {
 		return NULL;
 	}
 	return new_user;
 }
 
+//deletes a user
 void userFree(User user) {
 	user->fav_series = NULL;
+	user->friends = NULL;
 	free(user);
 	user = NULL;
 }
 
+//given a name of a series and a user, 
+//adds the series to the list of the user's favorite series
 MtmFlixResult userAddFavSeries(User user, const char* series_name) {
-	assert(user != NULL && series_name != NULL); //we check those arguments before passing them to the function
+	//we check those arguments before passing them to the function
+	assert(user != NULL && series_name != NULL);
 	setAdd(user->fav_series, (SetElement)series_name);
 	return MTMFLIX_SUCCESS;
 }
 
+//given a name of a series and a user, 
+//removes the series from the list of the user's favorite series
 MtmFlixResult userDeleteFavSeries(User user, const char* series_name) {
-	assert(user != NULL && series_name != NULL); //we check those arguments before passing them to the function
+	//we check those arguments before passing them to the function
+	assert(user != NULL && series_name != NULL);
 	setRemove(user->fav_series, (SetElement)series_name);
 	return MTMFLIX_SUCCESS;
 }
 
-
+//given a user instance (user1), and a name of another user
+//adds the second user to the first user's friend list
 MtmFlixResult userAddFriend(User user1, const char* username) {
-	assert(user1 != NULL && username != NULL); //we check those arguments before passing them to the function
+	//we check those arguments before passing them to the function
+	assert(user1 != NULL && username != NULL);
 	setAdd(user1->friends, (SetElement)username);
 	return MTMFLIX_SUCCESS;
 }
 
+//given a user instance (user1), and a name of another user,
+//deletes the second user from the first user's friend list
 MtmFlixResult userRemoveFriend(User user1, const char* username) {
-	assert(user1 != NULL && username != NULL); //we check those arguments before passing them to the function
+	//we check those arguments before passing them to the function
+	assert(user1 != NULL && username != NULL);
 	setRemove(user1->friends, (SetElement)username);
 	return MTMFLIX_SUCCESS;
 }
 
+//given a user instance, returns the user's favorite series
 Set userGetFavSeries(User user){
     if(user==NULL){
         return NULL;
@@ -75,6 +90,7 @@ Set userGetFavSeries(User user){
     return user->fav_series;
 }
 
+//given a user instance, returns the user's age
 int userGetAge(User user){
     if(user==NULL){
         return 0;
@@ -82,6 +98,7 @@ int userGetAge(User user){
     return user->age;
 }
 
+//given a user instance, returns the user's friends
 Set userGetFriends(User user){
     if(user==NULL){
         return NULL;

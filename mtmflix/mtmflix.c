f9765b8c7ac377c30b8ea20a679cbeb11a69969f
Dmitry Nikolaev <dsnikolaev@gmail.com>
2018-05-28 15:42:14 +0300

    in the dark

--- mtmflix/mtmflix.c
+++ mtmflix/mtmflix.c
@@ -242,11 +242,12 @@ MtmFlixResult mtmFlixSeriesJoin(MtmFlix mtmflix, const char* username,
 		seriesGetMaxAge(mapGet(mtmflix->series, (MapKeyElement)seriesName))) {
 		return MTMFLIX_USER_NOT_IN_THE_RIGHT_AGE;
 	}
- 	switch userAddFavSeries(mapGet(mtmflix->users, (MapKeyElement)username), 
- 		seriesName) {
+ 	switch (userAddFavSeries(mapGet(mtmflix->users, (MapKeyElement)username),
+ 		seriesName)) {
  		case MTMFLIX_SUCCESS : return MTMFLIX_SUCCESS;
  		case MTMFLIX_OUT_OF_MEMORY : exit(0);
  		case MTMFLIX_NULL_ARGUMENT : return MTMFLIX_NULL_ARGUMENT;
+        default: return MTMFLIX_SUCCESS;
  	}
  	//we won't get here
  	assert(false);
